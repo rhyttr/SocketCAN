bbd24f1252d96b25878b3739d9504308cd3ad3cf
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2007-05-18 19:48:54 +0000

    Updated CAN version to 20070518
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@312 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/include/linux/can/core.h
+++ kernel/2.6/include/linux/can/core.h
@@ -21,7 +21,7 @@
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
 
-#define CAN_VERSION "20070508"
+#define CAN_VERSION "20070518"
 
 /* increment this number each time you change some user-space interface */
 #define CAN_ABI_VERSION "8"
