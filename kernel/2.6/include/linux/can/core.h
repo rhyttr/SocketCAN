f39458579be41d1c61c099ade4da1f1351917695
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2007-07-07 06:54:42 +0000

    New version 20070707 (a prefered wedding date :) Changes since last version: - Reverted skb->iif changes - includes cleanup - vcan driver supports rtnl link API (kernel versions > 2.6.23)
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@413 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/include/linux/can/core.h
+++ kernel/2.6/include/linux/can/core.h
@@ -21,7 +21,7 @@
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
 
-#define CAN_VERSION "20070627"
+#define CAN_VERSION "20070707"
 
 /* increment this number each time you change some user-space interface */
 #define CAN_ABI_VERSION "8"
