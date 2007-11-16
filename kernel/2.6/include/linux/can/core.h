826b961cca97e675a2307a7f843effee23e9c092
thuermann <thuermann@030b6a49-0b11-0410-94ab-b0dab22257f2>
2007-11-16 13:38:16 +0000

    Update version string.
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@559 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/include/linux/can/core.h
+++ kernel/2.6/include/linux/can/core.h
@@ -21,7 +21,7 @@
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
 
-#define CAN_VERSION "20071027"
+#define CAN_VERSION "20071116"
 
 /* increment this number each time you change some user-space interface */
 #define CAN_ABI_VERSION "8"
