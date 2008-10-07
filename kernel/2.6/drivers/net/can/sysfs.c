e79e553be5647a4069e285f7bc2f663b2a3ceb8f
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2008-10-07 08:10:14 +0000

    Added strict_strtoul() from linux-2.6.25/lib/vsprintf.c to compile on older kernels < v2.6.25.
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@834 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/drivers/net/can/sysfs.c
+++ kernel/2.6/drivers/net/can/sysfs.c
@@ -29,6 +29,29 @@
 
 #include "sysfs.h"
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 25)
+int strict_strtoul(const char *cp, unsigned int base, unsigned long *res)
+{
+	char *tail;
+	unsigned long val;
+	size_t len;
+
+	*res = 0;
+	len = strlen(cp);
+	if (len == 0)
+		return -EINVAL;
+
+	val = simple_strtoul(cp, &tail, base);
+	if ((*tail == '\0') ||
+		((len == (size_t)(tail - cp) + 1) && (*tail == '\n'))) {
+		*res = val;
+		return 0;
+	}
+
+	return -EINVAL;
+}
+#endif
+
 #ifdef CONFIG_SYSFS
 
 /*
