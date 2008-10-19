d07a1650f1b403d064e8f9b9bbed1b8c8ba30490
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2008-10-19 16:12:47 +0000

    The callbacks in the struct can_priv used u32 for state and mode. There are already enums for these values. Use these enums to keep sparse quiet if a driver already uses these values.
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Oliver Hartkopp <oliver@hartkopp.net>
    Acked-by: Wolfgang Grandegger <wg@grandegger.com>
    
    
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@841 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/include/linux/can/dev.h
+++ kernel/2.6/include/linux/can/dev.h
@@ -111,10 +111,10 @@ struct can_priv {
 	struct sk_buff *echo_skb[CAN_ECHO_SKB_MAX];
 
 	int (*do_set_bittiming)(struct net_device *dev);
-	int (*do_get_state)(struct net_device *dev, u32 *state);
-	int (*do_set_mode)(struct net_device *dev, u32 mode);
+	int (*do_get_state)(struct net_device *dev, enum can_state *state);
+	int (*do_set_mode)(struct net_device *dev, enum can_mode mode);
 	int (*do_set_ctrlmode)(struct net_device *dev, u32 ctrlmode);
-	int (*do_get_ctrlmode)(struct net_device *dev, u32*ctrlmode);
+	int (*do_get_ctrlmode)(struct net_device *dev, u32 *ctrlmode);
 };
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,21)
