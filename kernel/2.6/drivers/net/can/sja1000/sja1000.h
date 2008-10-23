b816eaeee13f83c43df4ec6fac2ebfcc70852ea1
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2008-10-23 10:17:31 +0000

    Move sja1000 cdr and ocr defines into platform header as discussed on SocketCAN ML: https://lists.berlios.de/pipermail/socketcan-core/2008-October/002032.html
    Signed-off-by: Oliver Hartkopp <oliver.hartkopp@volkswagen.de>
    
    
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@847 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/drivers/net/can/sja1000/sja1000.h
+++ kernel/2.6/drivers/net/can/sja1000/sja1000.h
@@ -50,6 +50,7 @@
 #define SJA1000DEV_H
 
 #include <linux/can/dev.h>
+#include <linux/can/platform/sja1000.h>
 
 #define TX_TIMEOUT      (50*HZ/1000)	/* 50ms */
 #define RESTART_MS      100	/* restart chip on persistent errors in 100ms */
@@ -143,28 +144,6 @@
 #define ECC_FORM	0x40
 #define ECC_STUFF	0x80
 #define ECC_MASK	0xc0
-
-/* clock divider register */
-#define CDR_CLKOUT_MASK 0x07
-#define CDR_CLK_OFF	0x08 /* Clock off (CLKOUT pin) */
-#define CDR_RXINPEN	0x20 /* TX1 output is RX irq output */
-#define CDR_CBP		0x40 /* CAN input comparator bypass */
-#define CDR_PELICAN	0x80 /* PeliCAN mode */
-
-/* output control register */
-#define OCR_MODE_BIPHASE  0x00
-#define OCR_MODE_TEST     0x01
-#define OCR_MODE_NORMAL   0x02
-#define OCR_MODE_CLOCK    0x03
-#define OCR_TX0_INVERT    0x04
-#define OCR_TX0_PULLDOWN  0x08
-#define OCR_TX0_PULLUP    0x10
-#define OCR_TX0_PUSHPULL  0x18
-#define OCR_TX1_INVERT    0x20
-#define OCR_TX1_PULLDOWN  0x40
-#define OCR_TX1_PULLUP    0x80
-#define OCR_TX1_PUSHPULL  0xc0
-
 
 /*
  * Flags for sja1000priv.flags
