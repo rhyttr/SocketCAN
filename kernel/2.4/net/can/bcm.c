804f067ef408da41e4795c7830d66861ff4a7c28
hartkopp <hartkopp@030b6a49-0b11-0410-94ab-b0dab22257f2>
2008-12-19 16:28:02 +0000

    Backported fix for a race condition in bcm throttling.
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@884 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.4/net/can/bcm.c
+++ kernel/2.4/net/can/bcm.c
@@ -559,24 +559,22 @@ static void bcm_rx_timeout_handler(unsigned long data)
 }
 
 /*
- * bcm_rx_thr_handler - the time for blocked content updates is over now:
- *                      Check for throttled data and send it to the userspace
+ * bcm_rx_thr_flush - Check for throttled data and send it to the userspace
  */
-static void bcm_rx_thr_handler(unsigned long data)
+static int bcm_rx_thr_flush(struct bcm_op *op)
 {
-	struct bcm_op *op = (struct bcm_op *)data;
-	int i = 0;
-
-	/* mark disabled / consumed timer */
-	op->thrtimer.expires = 0;
+	int updated = 0;
 
 	if (op->nframes > 1) {
+		int i;
+
 		/* for MUX filter we start at index 1 */
 		for (i = 1; i < op->nframes; i++) {
 			if ((op->last_frames) &&
 			    (op->last_frames[i].can_dlc & RX_THR)) {
 				op->last_frames[i].can_dlc &= ~RX_THR;
 				bcm_rx_changed(op, &op->last_frames[i]);
+				updated++;
 			}
 		}
 
@@ -585,7 +583,26 @@ static void bcm_rx_thr_handler(unsigned long data)
 		if (op->last_frames && (op->last_frames[0].can_dlc & RX_THR)) {
 			op->last_frames[0].can_dlc &= ~RX_THR;
 			bcm_rx_changed(op, &op->last_frames[0]);
+			updated++;
 		}
+	}
+
+	return updated;
+}
+
+/*
+ * bcm_rx_thr_handler - the time for blocked content updates is over now:
+ *                      Check for throttled data and send it to the userspace
+ */
+static void bcm_rx_thr_handler(unsigned long data)
+{
+	struct bcm_op *op = (struct bcm_op *)data;
+
+	if (bcm_rx_thr_flush(op))
+		mod_timer(&op->thrtimer, jiffies + op->j_ival2);
+	else {
+		/* mark disabled / consumed timer */
+		op->thrtimer.expires = 0;
 	}
 }
 
@@ -1098,17 +1115,13 @@ static int bcm_rx_setup(struct bcm_msg_head *msg_head, struct msghdr *msg,
 			if (!op->j_ival1)
 				del_timer(&op->timer);
 
-			/* free currently blocked msgs ? */
-			if (op->thrtimer.expires) {
-				/* send blocked msgs hereafter */
-				mod_timer(&op->thrtimer, jiffies + 2);
-			}
-
 			/*
-			 * if (op->j_ival2) is zero, no (new) throttling
-			 * will happen. For details see functions
-			 * bcm_rx_update_and_send() and bcm_rx_thr_handler()
+			 * In any case cancel the throttle timer, flush
+			 * potentially blocked msgs and reset throttle handling
 			 */
+			del_timer(&op->thrtimer);
+			bcm_rx_thr_flush(op);
+			op->thrtimer.expires = 0;
 		}
 
 		if ((op->flags & STARTTIMER) && op->j_ival1)
