6cdec2a8f195de8b5dad7c651e0a81c5411f09a9
wolf <wolf@030b6a49-0b11-0410-94ab-b0dab22257f2>
2009-09-15 18:42:01 +0000

    sja1000: fix network statistics update
    The member "tx_bytes" of "struct net_device_stats" should be
    incremented when the interrupt is done and an "arbitration
    lost error" is a TX error and the statistics should be updated
    accordingly.
    
    See net-next-2.6 commit 8935f57e68c40c6469efe03f996bfb6150e18c6b
    
    Signed-off-by: Wolfgang Grandegger <wg@grandegger.com>
    
    
    git-svn-id: svn://svn.berlios.de/socketcan/trunk@1058 030b6a49-0b11-0410-94ab-b0dab22257f2

--- kernel/2.6/drivers/net/can/sja1000/sja1000.c
+++ kernel/2.6/drivers/net/can/sja1000/sja1000.c
@@ -245,11 +245,6 @@ static void chipset_init(struct net_device *dev)
 static int sja1000_start_xmit(struct sk_buff *skb, struct net_device *dev)
 {
 	struct sja1000_priv *priv = netdev_priv(dev);
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23)
-	struct net_device_stats *stats = can_get_stats(dev);
-#else
-	struct net_device_stats *stats = &dev->stats;
-#endif
 	struct can_frame *cf = (struct can_frame *)skb->data;
 	uint8_t fi;
 	uint8_t dlc;
@@ -283,7 +278,6 @@ static int sja1000_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	for (i = 0; i < dlc; i++)
 		priv->write_reg(priv, dreg++, cf->data[i]);
 
-	stats->tx_bytes += dlc;
 	dev->trans_start = jiffies;
 
 	can_put_echo_skb(skb, dev, 0);
@@ -443,7 +437,7 @@ static int sja1000_err(struct net_device *dev, uint8_t isrc, uint8_t status)
 		dev_dbg(ND2D(dev), "arbitration lost interrupt\n");
 		alc = priv->read_reg(priv, REG_ALC);
 		priv->can.can_stats.arbitration_lost++;
-		stats->rx_errors++;
+		stats->tx_errors++;
 		cf->can_id |= CAN_ERR_LOSTARB;
 		cf->data[0] = alc & 0x1f;
 	}
@@ -509,6 +503,7 @@ irqreturn_t sja1000_interrupt(int irq, void *dev_id)
 
 		if (isrc & IRQ_TI) {
 			/* transmission complete interrupt */
+		  	stats->tx_bytes += priv->read_reg(priv, REG_FI) & 0xf;
 			stats->tx_packets++;
 			can_get_echo_skb(dev, 0);
 			netif_wake_queue(dev);
